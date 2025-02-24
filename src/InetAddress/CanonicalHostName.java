43be80441e91609548ca5df3b1c166ef02f89789
Bishal Pulami <bishals01magar@gmail.com>
2025-02-24 23:28:53 +0545

    Canonical Host Name Program Added

--- src/InetAddress/CanonicalHostName.java
+++ src/InetAddress/CanonicalHostName.java
@@ -1,4 +1,22 @@
 package InetAddress;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class CanonicalHostName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an IP address or domain name: ");
+        String input = scanner.nextLine();
+
+        try {
+            InetAddress address = InetAddress.getByName(input);
+            System.out.println("Canonical Hostname: " + address.getCanonicalHostName());
+        } catch (UnknownHostException e) {
+            System.out.println("Could not resolve hostname.");
+            e.printStackTrace();
+        }
+    }
 }
+
