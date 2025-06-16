b26a39a5145a67d9b437d4147368b46ed1b03b39
Bishal Pulami <bishals01magar@gmail.com>
2025-06-16 13:55:00 +0545

    Add program to check reachability of a host using InetAddress

---
+++ src/InetAddress/Reachability.java
@@ -0,0 +1,10 @@
+package InetAddress;
+
+import java.net.InetAddress;
+
+public class Reachability {
+    public static void main(String[] args) throws Exception {
+        InetAddress ip = InetAddress.getByName("www.mechicampus.edu.np");
+        System.out.println("Is reachable: " + ip.isReachable(3000));
+    }
+}
