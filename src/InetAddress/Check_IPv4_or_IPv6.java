2c5e46b529a7c6264d6bb6b993c75f5dbeb3288b
Bishal Pulami <bishals01magar@gmail.com>
2025-06-18 14:52:59 +0545

    Program to check IP address (IPV4 or IPV6)

--- src/InetAddress/Check_IPv4_or_IPv6.java
+++ src/InetAddress/Check_IPv4_or_IPv6.java
@@ -3,14 +3,23 @@ package InetAddress;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Check_IPv4_or_IPv6 {
     public static void main(String[] args) throws Exception {
-        InetAddress ip = InetAddress.getByName("2400:cb00:2048:1::c629:d7a2");
-        if (ip instanceof Inet4Address) {
-            System.out.println("IPv4 Address");
-        } else if (ip instanceof Inet6Address) {
-            System.out.println("IPv6 Address");
+
+        try{
+            InetAddress address = InetAddress.getByName("2400:cb00:2048:1::c629:d7a2");
+            if (address instanceof Inet4Address) {
+                System.out.println("IPv4 Address");
+            } else if (address instanceof Inet6Address) {
+                System.out.println("IPv6 Address");
+            }
+        }catch (UnknownHostException e){
+            System.err.println("Cannot solve the host name");
+            e.printStackTrace();
         }
+
+
     }
 }
