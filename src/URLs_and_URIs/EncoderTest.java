2d4b8ba745c98586702a814a1e919dca81074730
Bishal Pulami <bishals01magar@gmail.com>
2025-06-20 14:39:44 +0545

    Java program to encode a URI

---
+++ src/URLs_and_URIs/EncoderTest.java
@@ -0,0 +1,25 @@
+//x-www-form-urlencoded strings
+
+package URLs_and_URIs;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class EncoderTest {
+    public static void main(String[] args) {
+        System.out.println(URLEncoder.encode("This string has spaces", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This*string*has*asterisks", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This%string%has%percent %signs", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This+string+has+pluses", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This/string/has/slashes", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This\"string\"has\"quote\"marks", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This:string:has:colons", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This~string~has~tildes", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This(string)has(parentheses)", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This.string.has.periods", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This=string=has=equals=sig ns", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("This&string&has&ampersands", StandardCharsets.UTF_8));
+        System.out.println(URLEncoder.encode("Thiséstringéhasé non-ASCII characters", StandardCharsets.UTF_8));
+    }
+}
