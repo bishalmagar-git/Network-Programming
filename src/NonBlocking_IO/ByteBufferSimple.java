a279f5b967f58464859d4c5bd35acb8b2aaa5771
Bishal Pulami <bishals01magar@gmail.com>
2025-06-24 18:35:00 +0545

    Simple ByteBuffer Example (Create → Fill → Flip → Drain) java Copy code

---
+++ src/NonBlocking_IO/ByteBufferSimple.java
@@ -0,0 +1,21 @@
+package NonBlocking_IO;
+
+import java.nio.ByteBuffer;
+
+public class ByteBufferSimple {
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+
+        buffer.put((byte)'H');
+        buffer.put((byte)'I');
+
+        buffer.flip(); //Switch to read mode
+
+        //Read characters from buffer
+        while (buffer.hasRemaining()){
+            System.out.println((char) buffer.get());  //Drain
+        }
+
+    }
+}
+
